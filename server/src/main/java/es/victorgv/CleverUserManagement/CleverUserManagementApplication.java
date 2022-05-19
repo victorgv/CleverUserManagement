8251ea6d6f7c767c497c6648fa5b46def81b566a
victorgv <victorgv@gmail.com>
2022-05-19 22:04:51 +0200

    spring starter

---
+++ server/src/main/java/es/victorgv/CleverUserManagement/CleverUserManagementApplication.java
@@ -0,0 +1,13 @@
+package es.victorgv.CleverUserManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleverUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleverUserManagementApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/es/victorgv/CleverUserManagement/CleverUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package es.victorgv.CleverUserManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleverUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
