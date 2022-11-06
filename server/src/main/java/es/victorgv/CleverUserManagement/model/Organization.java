9184ebabb2a8188a5f595267b507da1f4886a3fd
victorgv <victorgv@gmail.com>
2022-11-06 19:52:07 +0100

    añadido modelo 'Organization'

---
+++ server/src/main/java/es/victorgv/CleverUserManagement/model/Organization.java
@@ -0,0 +1,47 @@
+package es.victorgv.CleverUserManagement.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Setter @Getter @NoArgsConstructor
+public class Organization {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID organizationUUID;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 100, nullable = true)
+    private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Organization that = (Organization) o;
+
+        if (!organizationUUID.equals(that.organizationUUID)) return false;
+        if (!name.equals(that.name)) return false;
+        return description != null ? description.equals(that.description) : that.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = organizationUUID.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+}
